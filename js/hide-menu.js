ca4e053bc58fc8b4a3c990f276149eb5c983d9f3
ZooMG1N <zoomg1next@gmail.com>
2018-01-21 20:42:27 +0200

    add project

---
+++ js/hide-menu.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.hamburger-menu').click(function() {
+//        $('.hide-menu').slideToggle()
+        $('.hide-menu').slideToggle().css('display', 'flex');
+    })
+})
